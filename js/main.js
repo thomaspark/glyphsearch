3468e1f7db9785860fb2a14ed1c6cf315396ac9b
Thomas Park <thomas@thomaspark.me>
2013-11-03 01:23:04 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// $.getJSON("./data/icons.json", function(data) {
+//		var compiledTemplate = Handlebars.templates['template'],
+//		html = compiledTemplate(data),
+//		$("#entries").append(html);
+// });
+
+$(".entry").click(function(){
+	var name = $(this).find(".description").text();
+	window.prompt ("Copy to clipboard:", name);
+});
+
+$("#search")
+	.keyup(function(e) {
+		if (e.keyCode == 27) {
+			$(this).val("").change();
+		}
+	})
+	.focus(function(){
+		$(this).attr("placeholder", "");
+	})
+	.blur(function(){
+		$(this).attr("placeholder", "Search");
+	})
+	.fastLiveFilter('#entries .row');
+
+$(".search-term").click(function(){
+	$("#search").val($(this).text()).focus().change();
+});
+
+var filters = $("#filter a");
+
+filters.click(function(){
+	var filter = $(this).attr("data-filter");
+
+	filters.removeClass("active");
+	$(this).addClass("active");
+	$("#filter-label").text($(this).text());
+
+	if (filter == "all"){
+		$(".section").removeClass("hide");
+		return;
+	} else {
+		$(".section").addClass("hide");
+		$("." + filter).removeClass("hide");
+	}
+});
