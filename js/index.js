a81a03de3c9141fa268895fc854351cb51c6c836
Thomas Park <thomas@thomaspark.me>
2013-11-18 14:04:36 -0500

    use batch.json to load all icons

--- js/index.js
+++ js/index.js
@@ -7,7 +7,15 @@ var index = new AlgoliaSearch("9JQV0RIHU0", "2219d421236cba4cf37a98e7f97b3ec5").
 		'<div class="clearfix"></div>' +
 		'</div>',
 	innerTemplateCompiled = Hogan.compile(innerTemplate),
-	$filters = $("#filter a");
+	allTemplate = '<div class="entry col-lg-1 col-md-2 col-sm-3 col-xs-4">' +
+		'<div class="description">{{{class}}}</div>' +
+		'<div class="thumb"><i class="{{{class}}}"></i></div>' +
+		'<div class="name">{{{name}}}</div>' +
+		'<div class="tags hidden-xs">{{{tags}}}</div>' +
+		'<div class="clearfix"></div>' +
+		'</div>',
+	allTemplateCompiled = Hogan.compile(allTemplate),
+	icons = {};
 
 function search(v) {
 	v = $.trim(v);
@@ -16,61 +24,95 @@ function search(v) {
 		// do not perform the same query twice, results will not change
 		return;
 	}
+
 	$('#search').data('q', v);
 
-	index.search(v, function(success, content) {
-		if (!success) {
-			return;
-		}
+	if (v.length === 0) {
+		load(icons);
+	} else {
+		index.search(v, function(success, content) {
+			if (!success) return;
 
-		$('.section.row').empty();
-		for (var i = 0; i < content.hits.length; ++i) {
-			var hit = content.hits[i];
-			if (hit.name) {
-				$('#' + hit._tags[0]).append(innerTemplateCompiled.render(hit));
-			}
-		}
-	}, { hitsPerPage: 1000 });
+			var result = {};
+			generate(content.hits, innerTemplateCompiled, result);
+
+		}, {hitsPerPage: 1000});
+	}
 }
 
-$('#search').change(function() {
-	search($(this).val());
-}).keyup(function(e) {
-	if (e.which == 27) {
-		$(this).val('').change();
-		return;
+function generate(data, template, output) {
+	data.forEach(function(v, i){
+		var lib,
+			html;
+
+		if (v.name) {
+			lib = v._tags[0];
+			html = template.render(v);
+
+			if (!output[lib]) {
+				output[lib] = html;
+			} else {
+				output[lib] += html;
+			}
+		}
+	});
+
+	load(output);
+}
+
+function load(htmls) {
+	$('.section.row').empty();
+
+	for (var i in htmls) {
+		$('#' + i).html(htmls[i]);
 	}
-	search($(this).val());
-}).focus(function(){
-	$(this).attr("placeholder", "");
-}).blur(function(){
-	$(this).attr("placeholder", "Search");
+}
+
+function handlers() {
+	var $filters = $("#filter a");
+
+	$('#search').change(function() {
+		search($(this).val());
+	}).keyup(function(e) {
+		if (e.which == 27) {
+			$(this).val('').change();
+			return;
+		}
+		search($(this).val());
+	}).focus(function(){
+		$(this).attr("placeholder", "");
+	}).blur(function(){
+		$(this).attr("placeholder", "Search");
+	});
+
+	$(".search-term").click(function() {
+		$("#search").val($(this).text()).change();
+	});
+
+	$(".section.row").on("click", ".entry", function(){
+		var name = $(this).find(".description").text();
+		window.prompt("Copy to clipboard:", name);
+	});
+
+	$filters.click(function(e){
+		e.preventDefault();
+		var filter = $(this).attr("data-filter");
+
+		$filters.removeClass("active");
+		$(this).addClass("active");
+		$("#filter-label").text($(this).text());
+
+		if (filter == "all"){
+			$(".section").removeClass("hide");
+			return;
+		} else {
+			$(".section").addClass("hide");
+			$(".section-" + filter).removeClass("hide");
+		}
+	});
+}
+
+$.getJSON("./data/batch.json", function(data) {
+	generate(data, allTemplateCompiled, icons);
+	handlers();
 });
-
-$(".search-term").click(function() {
-	$("#search").val($(this).text()).change();
-});
-
-$(".section.row").on("click", ".entry", function(){
-	var name = $(this).find(".description").text();
-	window.prompt("Copy to clipboard:", name);
-});
-
-$filters.click(function(e){
-	e.preventDefault();
-	var filter = $(this).attr("data-filter");
-
-	$filters.removeClass("active");
-	$(this).addClass("active");
-	$("#filter-label").text($(this).text());
-
-	if (filter == "all"){
-		$(".section").removeClass("hide");
-		return;
-	} else {
-		$(".section").addClass("hide");
-		$(".section-" + filter).removeClass("hide");
-	}
-});
-
-search('');
