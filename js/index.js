beecd8d6b21ef99fed8879c0055ffac7895b7148
Thomas Park <thomas@thomaspark.me>
2013-12-04 12:20:22 -0500

    js cleanup

--- js/index.js
+++ js/index.js
@@ -15,7 +15,8 @@ var index = new AlgoliaSearch("9JQV0RIHU0", "2219d421236cba4cf37a98e7f97b3ec5").
     '<div class="clearfix"></div>' +
     '</div>',
   allTemplateCompiled = Hogan.compile(allTemplate),
-  icons = {};
+  icons = {},
+  flashEnabled = false;
 
 function search(v) {
   v = $.trim(v);
@@ -107,45 +108,43 @@ function handlers() {
       $(".section-" + filter).removeClass("hide");
     }
   });
+
+  ZeroClipboard.setDefaults({
+    moviePath: "bower_components/zeroclipboard/ZeroClipboard.swf",
+    forceHandCursor: true
+  });
+
+  var clip = new ZeroClipboard();
+
+  clip.on("load", function() {
+    flashEnabled = true;
+
+    clip.on("complete", function() {
+      var messages = ["COPIED!", "GOT IT!", "PASTE ME!"];
+      var colors = ["#1abc9c", "#2ecc71", "#9b59b6", "#3498db", "#34495e", "#e74c3c"];
+      var randomTextNum = Math.floor(Math.random() * messages.length);
+      var randomColorNum = Math.floor(Math.random() * colors.length);
+      $(".copied div").html(messages[randomTextNum]);
+      $(".copied").css("background-color", colors[randomColorNum]).show().find("div").addClass("animateIn");
+      setTimeout('$(".copied div").removeClass("animateIn").addClass("animateOut");$(".copied").fadeOut(function(){$(".copied div").removeClass("animateOut")})', 700);
+    });
+
+    clip.on("mouseout", function() {
+      $(".entry").removeClass("zeroclipboard-is-hover");
+    });
+
+    clip.glue($(".entry"));
+  });
+
+  clip.on("noflash", function() {
+    $(".section.row").on("click", ".entry", function(){
+      var name = $(this).find(".description").text();
+      window.prompt("Copy to clipboard:", name);
+    });
+  });
 }
 
 $.getJSON("./data/batch.json", function(data) {
   generate(data, allTemplateCompiled, icons);
   handlers();
 });
-
-ZeroClipboard.setDefaults({
-  moviePath: "bower_components/zeroclipboard/ZeroClipboard.swf",
-  forceHandCursor: true
-});
-
-var clip = new ZeroClipboard();
-var flashEnabled;
-
-//The complete event is fired when the text is successfully copied to the clipboard.
-clip.on("load",function() {
-  flashEnabled = true;
-  // glue the element with the flash movie for copying
-  clip.glue( $(".entry") );
-  clip.on("complete", function(client, args) {
-    var messages = ["COPIED!", "GOT IT!", "PASTE ME!"];
-    var colors = ["#1abc9c", "#2ecc71", "#9b59b6", "#3498db", "#34495e", "#e74c3c"];
-      var randomTextNum = Math.floor(Math.random() * messages.length);
-      var randomColorNum = Math.floor(Math.random() * colors.length);
-      $(".copied div").html(messages[randomTextNum]);
-      $(".copied").css("background-color", colors[randomColorNum]).show().find("div").addClass("animateIn");
-      setTimeout('$(".copied div").removeClass("animateIn").addClass("animateOut");$(".copied").fadeOut(function(){$(".copied div").removeClass("animateOut")})', 700);
-  });
-  //The mouseout event is fired when the user's mouse pointer leaves the Flash movie.
-  clip.on( "mouseout", function(client, args) {
-      $(".entry").removeClass("zeroclipboard-is-hover");
-  });
-});
-//The noflash event is fired when the user doesn't have flash installed on their system.
-clip.on( "noflash", function (client, args) {
-  flashEnabled = false;
-    $(".section.row").on("click", ".entry", function(){
-    var name = $(this).find(".description").text();
-    window.prompt("Copy to clipboard:", name);
-  });
-} );
--- js/offline.js
+++ js/offline.js
@@ -1,37 +1,5 @@
 var $filters = $("#filter a");
 
-ZeroClipboard.setDefaults({
-  moviePath: "bower_components/zeroclipboard/ZeroClipboard.swf",
-  forceHandCursor: true
-});
-
-var clip = new ZeroClipboard();
-//The complete event is fired when the text is successfully copied to the clipboard.
-clip.on("load",function() {
-  clip.on("complete", function(client, args) {
-    var messages = ["COPIED!", "GOT IT!", "PASTE ME!"];
-    var colors = ["#1abc9c", "#2ecc71", "#9b59b6", "#3498db", "#34495e", "#e74c3c"];
-      var randomTextNum = Math.floor(Math.random() * messages.length);
-      var randomColorNum = Math.floor(Math.random() * colors.length);
-      $(".copied div").html(messages[randomTextNum]);
-      $(".copied").css("background-color", colors[randomColorNum]).show().find("div").addClass("animateIn");
-      setTimeout('$(".copied div").removeClass("animateIn").addClass("animateOut");$(".copied").fadeOut(function(){$(".copied div").removeClass("animateOut")})', 700);
-  });
-  //The mouseout event is fired when the user's mouse pointer leaves the Flash movie.
-  clip.on( "mouseout", function(client, args) {
-      $(".entry").removeClass("zeroclipboard-is-hover");
-  });
-  // glue the element with the flash movie for copying
-  clip.glue( $(".entry") );
-})
-//The noflash event is fired when the user doesn't have flash installed on their system.
-clip.on( "noflash", function ( client, args ) {
-    $(".section.row").on("click", ".entry", function(){
-    var name = $(this).find(".description").text();
-    window.prompt("Copy to clipboard:", name);
-  });
-} );
-
 $("#search")
   .keyup(function(e) {
     if (e.keyCode == 27) {
@@ -65,4 +33,36 @@ $filters.click(function(e){
     $(".section").addClass("hide");
     $(".section-" + filter).removeClass("hide");
   }
+});
+
+ZeroClipboard.setDefaults({
+  moviePath: "bower_components/zeroclipboard/ZeroClipboard.swf",
+  forceHandCursor: true
+});
+
+var clip = new ZeroClipboard();
+
+clip.on("load", function() {
+  clip.on("complete", function() {
+    var messages = ["COPIED!", "GOT IT!", "PASTE ME!"];
+    var colors = ["#1abc9c", "#2ecc71", "#9b59b6", "#3498db", "#34495e", "#e74c3c"];
+    var randomTextNum = Math.floor(Math.random() * messages.length);
+    var randomColorNum = Math.floor(Math.random() * colors.length);
+    $(".copied div").html(messages[randomTextNum]);
+    $(".copied").css("background-color", colors[randomColorNum]).show().find("div").addClass("animateIn");
+    setTimeout('$(".copied div").removeClass("animateIn").addClass("animateOut");$(".copied").fadeOut(function(){$(".copied div").removeClass("animateOut")})', 700);
+  });
+
+  clip.on("mouseout", function() {
+    $(".entry").removeClass("zeroclipboard-is-hover");
+  });
+
+  clip.glue($(".entry"));
+});
+
+clip.on( "noflash", function() {
+  $(".section.row").on("click", ".entry", function(){
+    var name = $(this).find(".description").text();
+    window.prompt("Copy to clipboard:", name);
+  });
 });
