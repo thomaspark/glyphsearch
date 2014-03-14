95cbe765a20d91e867db2f9a9ea32a411945d502
Thomas Park <thomas@thomaspark.me>
2014-03-14 18:40:03 -0400

    split icons json file by library

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,28 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    'merge-json': {
+        icons: {
+            src: [ "data/icons-*.json" ],
+            dest: "data/icons.json"
+        }
+    },
     'compile-handlebars': {
         remote: {
           template: 'templates/batch.handlebars',
           templateData: 'data/icons.json',
-          output: './data/batch.json'
+          output: 'data/batch.json'
         }
     }
   });
 
+  grunt.loadNpmTasks('grunt-merge-json');
   grunt.loadNpmTasks('grunt-compile-handlebars');
 
-  grunt.registerTask('build', 'compile-handlebars');
   grunt.registerTask('default', function(){
     grunt.task.run('build');
   });
+
+  grunt.registerTask('build', ['merge-json', 'compile-handlebars']);
 
   grunt.registerTask('index', 'Push batch.json to Algolia\'s server', function() {
     // required api key
